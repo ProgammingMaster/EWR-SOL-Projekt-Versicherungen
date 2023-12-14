d3080008535e8063d6fd3f85c5bb5822da950e31
ProgammingMaster <151567483+ProgammingMaster@users.noreply.github.com>
2023-12-14 20:53:29 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -20,3 +20,108 @@ const abspannElement = document.getElementById('abspannElement');
     }
 
     setInterval(changeText, 3000); // Ändert den Text alle 3 Sekunden
+
+const questions = [
+    {
+        question: "Wofür gibt es Sachversicherungen?",
+        answers: [
+            {text: "'Risiken, die sich auf einzelne Personen beziehen.'", correct: false},
+            {text: "'Risiken, die sich auf Eigentümer beziehen.'", correct: true},
+            {text: "'Risiken, die sich auf fremde Handys beziehen.'", correct: false},
+        ]
+    },
+    {
+        question: "Was bedeutet 'Versicherungsprämie'?",
+        answers: [
+            {text: "Der Preis für die jeweilige Versicherung", correct: true},
+            {text: "Das Geld, das man von der Versicherung bei einem Schadensfall bezahlt bekommt", correct: false},
+            {text: "Die Vertragsdetails zur jeweiligen Versicherung", correct: false},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);   
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Agian";
+    nextButton.style.display = "bock";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }
+    else{
+        startQuiz();
+    }
+});
+
+startQuiz();
